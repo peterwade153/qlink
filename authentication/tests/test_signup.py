13d68a3340a56e8c2fbfbfa56ebf38c446f42cb1
peterwade153 <peterwade153@gmail.com>
2021-08-04 12:31:26 +0300

    Add auth signup tests

---
+++ authentication/tests/test_signup.py
@@ -0,0 +1,26 @@
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+from authentication.models import User
+
+
+class RegisterAPITestCase(APITestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(email='jacob@gmail.com', password='Top_secret')
+
+    def test_register_user(self):
+        data = {
+            'email':'test@gmail.com',
+            'password':'Passtest123'
+        }
+        response = self.client.post('/auth/users/', data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_register_existing_user(self):
+        data = {
+            'email':'jacob@gmail.com', 
+            'password':'Top_secret'
+        }
+        response = self.client.post('/auth/users/', data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
