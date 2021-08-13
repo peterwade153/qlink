621a12a75c2785f71c7f1b1d8b33f0c961de2253
peterwade153 <peterwade153@gmail.com>
2021-08-13 10:46:08 +0300

    Add django rest auth for authene

--- app/settings.py
+++ app/settings.py
@@ -37,7 +37,18 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'authentication',
+    'django.contrib.sites',
+
+    'rest_framework',
+    'rest_framework.authtoken',
+
+    'allauth',
+    'allauth.account',
+
+    'rest_auth',
+    'rest_auth.registration',
+
+    # local apps
     'backend',
 ]
 
@@ -130,7 +141,18 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-AUTH_USER_MODEL = 'authentication.User'
+ACCOUNT_AUTHENTICATION_METHOD = "email"
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_USERNAME_REQUIRED = False
 
-# Time after which auth token expires
-AUTH_TOKEN_EXPIRE_HOURS = 48
+AUTHENTICATION_BACKENDS = [
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+SITE_ID = 1
--- authentication/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from authentication import views
-
-
-urlpatterns = [
-    path('users/', views.UserView.as_view(), name='users')
-]
