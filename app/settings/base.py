9beb7d3f9a45ac096e8239774672402b98fb29d7
peterwade153 <peterwade153@gmail.com>
2021-09-20 13:07:28 +0300

    heroku deployment allowed hosts

--- app/settings/base.py
+++ app/settings/base.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'jisjajajsocoasjoCVUASDV')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', False)
 
-ALLOWED_HOSTS = ['https://qlink-api.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['qlink-api.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
