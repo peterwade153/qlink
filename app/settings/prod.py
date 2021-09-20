a8bc1b003bb3b3fa72bf61252d9f32d20b056525
peterwade153 <peterwade153@gmail.com>
2021-09-20 13:18:47 +0300

    update databse settings

--- app/settings/prod.py
+++ app/settings/prod.py
@@ -2,11 +2,5 @@ import dj_database_url
 from .base import *
 
 
-DATABASES = {
-    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
-}
-
-DATABASES['default'].update(dj_database_url.config(
-    conn_max_age=500, ssl_require=True),
-    engine='heroku_connect.db.backends.postgres',
-)
+DATABASES = {}
+DATABASES['default'] = dj_database_url.config(conn_max_age=500, ssl_require=True)
